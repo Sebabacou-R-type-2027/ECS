b824aa5e6c5e7be8113290deb605e152f745478c
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-11-01 13:38:53 +0100

    feat(main) Add default systems setup

---
+++ src/core.cpp
@@ -0,0 +1,26 @@
+module ecs;
+import :core;
+import :components;
+import :components.engine;
+import :components.gui;
+import :systems;
+import :systems.engine;
+import :systems.gui;
+
+namespace ecs {
+    void registry::register_gui_systems() noexcept
+    {
+        this->register_system<components::gui::display>(systems::gui::display);
+        this->register_system<components::gui::animation_clock>(components::clock::reset_system);
+        this->register_system<const components::gui::drawable>(systems::gui::draw);
+        this->register_system<components::gui::animation_clock>(components::clock::cache_system);
+        this->register_system<components::gui::drawable, const components::position>(systems::gui::reposition);
+        this->register_system<components::gui::display>(systems::gui::clear);
+    }
+
+    void registry::register_engine_systems() noexcept
+    {
+        this->register_system<components::position, const components::engine::velocity>(systems::engine::movement);
+        this->register_system<components::position, const components::engine::controllable>(systems::engine::control);
+    }
+}
