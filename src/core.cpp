60e9e9598f3d71f5601d855e92d5123681b7bc55
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-11-03 03:41:05 +0100

    refactor(main) Remove shader drawing system from default and move clear after display to allow for third-party draw systems

--- src/core.cpp
+++ src/core.cpp
@@ -10,13 +10,12 @@ import :systems.gui;
 namespace ecs {
     void registry::register_gui_systems() noexcept
     {
+        this->register_system<components::gui::display>(systems::gui::clear);
         this->register_system<components::gui::display>(systems::gui::display);
         this->register_system<components::gui::animation_clock>(components::clock::reset_system);
         this->register_system<const components::gui::drawable>(systems::gui::draw);
         this->register_system<components::gui::animation_clock>(components::clock::cache_system);
         this->register_system<components::gui::drawable, const components::position>(systems::gui::reposition);
-        this->register_system<ecs::components::gui::display>(ecs::systems::gui::draw_shader_background);
-        this->register_system<components::gui::display>(systems::gui::clear);
     }
 
     void registry::register_engine_systems() noexcept
